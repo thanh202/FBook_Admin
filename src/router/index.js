6bc2f7feadf7e8306ca1cd7ca6d74933473ebcbd
Thanh202 <lethanh27112002@gmail.com>
2023-11-14 11:00:42 +0700

    firt commit

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+// Composables
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  {
+    path: "/admin",
+    component: () => import("@/layouts/default/Default.vue"),
+    children: [
+      {
+        path: "",
+        name: "Home",
+        component: () => import("@/views/Home.vue"),
+      },
+    ],
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
